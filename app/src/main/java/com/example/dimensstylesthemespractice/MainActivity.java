66637a4d5da94033dd7f514b1df6900f42960127
CTaylor1571 <90778109+CTaylor1571@users.noreply.github.com>
2022-09-09 08:09:12 -0500

    Almost finished UI, using themes.xml, and started working on functionality

--- app/src/main/java/com/example/dimensstylesthemespractice/MainActivity.java
+++ app/src/main/java/com/example/dimensstylesthemespractice/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.dimensstylesthemespractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     // this is a change
+    String name;
+    EditText nameTextBox;
+    public void onButtonClick(View view){
+        name = nameTextBox.getText().toString();
+        
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name="";
+        nameTextBox = findViewById(R.id.editTextTextPersonName);
     }
 }
