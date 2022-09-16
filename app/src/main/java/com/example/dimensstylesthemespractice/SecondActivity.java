c4a155ce9b05c330acdf26719755b8012cb09bc0
CTaylor1571 <90778109+CTaylor1571@users.noreply.github.com>
2022-09-16 08:19:22 -0500

    Finished app in UI and functionality

--- app/src/main/java/com/example/dimensstylesthemespractice/SecondActivity.java
+++ app/src/main/java/com/example/dimensstylesthemespractice/SecondActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     // this is a change
     String name;
+    TextView textView;
 
 
 
@@ -19,5 +21,24 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_activity);
         Intent intent1=getIntent();
         name= intent1.getStringExtra("NAME");
+        textView=findViewById(R.id.nameRateResultTextView);
+        rateName(name);
+
+    }
+    public void rateName(String name){
+        if(name.equalsIgnoreCase("Christian")||name.equalsIgnoreCase("Christian Taylor")) {
+            textView.setText("10/10, best name by far :)");
+        } else if (name.equalsIgnoreCase("Aditya")||name.equalsIgnoreCase("Aditya gupta")) {
+            textView.setText("2/10, pretty bad name :(");
+        } else if (name.equals("")) {
+            textView.setText("Enter a name, silly :P");
+        } else {
+            int rand = (int)(Math.random()*10)+1;
+            textView.setText("This name is a "+ rand + "/10 :)");
+        }
+    }
+    public void switchScreens(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
